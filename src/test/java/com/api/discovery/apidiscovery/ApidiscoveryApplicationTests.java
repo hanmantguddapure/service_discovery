7e74a46fd9c2e31a6aa1c47e440bed495d59215b
hanmantguddapure <hanmantguddapure@gmail.com>
2022-07-19 21:38:41 +0530

    first commit

---
+++ src/test/java/com/api/discovery/apidiscovery/ApidiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.discovery.apidiscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApidiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
